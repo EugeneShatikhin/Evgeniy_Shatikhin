d5de88754636f7c67f7b536af89f80ee2b29d0ed
Eugene <yevhenii.shatikhin@gmail.com>
2021-10-27 20:20:14 +0300

    Completed Extra Task 2

---
+++ Extra2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DevAndTest_Classes
+{
+    [TestFixture]
+    class Extra2
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(2149583361, GetInt32FromIPv4("128.32.10.1"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(32, GetInt32FromIPv4("0.0.0.32"));
+        }
+        public UInt32 GetInt32FromIPv4(string ip)
+        {
+            var octets = ip.Split('.').Reverse();
+            string sresult = "";
+            foreach(var octet in octets)
+            {
+                string binary = Convert.ToString(int.Parse(octet), 2);
+                if (binary.Length < 8) binary = binary.Insert(0, string.Concat(Enumerable.Repeat('0', 8 - binary.Length)));
+                sresult = sresult.Insert(0, binary);
+            }
+            return Convert.ToUInt32(sresult, 2);
+        }
+    }
+}
