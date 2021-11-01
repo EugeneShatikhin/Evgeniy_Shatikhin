c295048f768905cfaa00e4224550673195d10ad4
Eugene <yevhenii.shatikhin@gmail.com>
2021-11-01 20:25:46 +0200

    Fixed Task4

--- Task4.cs
+++ Task4.cs
@@ -18,15 +18,28 @@ namespace DevAndTest_Classes
         {
             Assert.AreEqual(4, NumOfPairs(new int[] { 1, 3, 6, 2, 7, 8, 4, 5 }, 9));
         }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(6, NumOfPairs(new int[] { 3, 3, 3, 3 }, 6));
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(3, NumOfPairs(new int[] { -2, -2, -2 }, -4));
+        }
         public int NumOfPairs(int[] nums, int target)
         {
             var numbers = nums.ToList();
             int pairs = 0;
-            for (int i = 0; i < nums.Length; i++)
+            int length = nums.Length;
+            for (int i = 0; i < length; i++)
             {
-                pairs += numbers.Count(num => target - numbers[i] == num);
+                pairs += numbers.Count(num => num + numbers[0] == target);
+                if(numbers[0] * 2 == target) pairs -= 1; // we don't want to count num+num as pair
+                numbers.RemoveAt(0);
             }
-            return pairs / 2;
+            return pairs;
         }
     }
 }
